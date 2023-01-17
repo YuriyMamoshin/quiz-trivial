759b91e703516cd9ed24432cc1538eb51eb214e2
YuriyMamoshin <122636215+YuriyMamoshin@users.noreply.github.com>
2023-01-17 08:17:19 +0200

    Create the main page

---
+++ src/App.js
@@ -0,0 +1,12 @@
+export default function App() {
+    return (
+        <main>
+<div className="content">
+<h1 className="caption">Quizzical</h1>
+<p className="description">Some description if needed</p>
+<button>Start quiz</button>
+</div>
+        </main>
+
+    )
+}
