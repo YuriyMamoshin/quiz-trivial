2113fe348cc375439446aa2d7595b5591f865b14
YuriyMamoshin <122636215+YuriyMamoshin@users.noreply.github.com>
2023-01-28 04:59:59 +0200

    Added some styling

--- src/Answer.js
+++ src/Answer.js
@@ -2,7 +2,7 @@
 
 export default function Answer(props) {
 const styles = {
-    backgroundColor: props.clicked ? "#D6DBF5" : "white"
+    backgroundColor: props.clicked ? "#D6DBF5" : ""
 }
     return (
         <button style={styles}   className="answer-button" onClick={props.choose}>{props.value}{props.correct}</button>
